2df137952a2d2e1ecf48f3e912075ec464530a74
ImprovKing <philipalexander572@gmail.com>
2025-01-06 11:49:12 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,45 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lazStack';
+
+  myForm: FormGroup;
+  storedData: any;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      Name: [''],
+      Username: [''],
+      Password: [''] ,
+      Email: [''] ,
+      PresentAddress: [''] ,
+      DateoFBirth: [''] ,
+      PermanentAddress: [''] ,
+      City: [''] ,
+      Country : [''],
+      PostalCode : [''],
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadStoredData();
+  }
+
+  loadStoredData() {
+    const data = localStorage.getItem('formData');
+    if (data) {
+      this.storedData = JSON.parse(data);
+      this.myForm.patchValue(this.storedData);
+    }
+  }
+
+  onSubmit() {
+    const formData = this.myForm.value;
+    localStorage.setItem('formData', JSON.stringify(formData));
+    this.loadStoredData(); // Refresh the displayed data
+  }
+
+  onEdit() {
+    this.storedData = this.myForm.value; // Update stored data with current form values
+  }
+  
 }
